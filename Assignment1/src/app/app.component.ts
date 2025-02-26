01a03e7bdea04c1b7db1420e7b0748c30771be56
devsojitra37 <devanshsojitra37@gmail.com>
2025-02-26 10:29:12 +0530

    Added Employee, User, and Admin components

--- Assignment1/src/app/app.component.ts
+++ Assignment1/src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AdminComponent } from './component/admin/admin.component';
+import { EmployeeComponent } from './component/employee/employee.component';
+import { UserComponent } from './component/user/user.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,AdminComponent,EmployeeComponent,UserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
